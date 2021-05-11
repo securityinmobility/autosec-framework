939353e5b4eb9c0751c7f91635013a49231a9f13
Marco Michl <marco.michl@carissma.eu>
2021-05-11 17:09:42 +0200

    Further work on the io / logging component

--- core/io.py
+++ core/io.py
@@ -11,18 +11,15 @@ class Color(Enum):
     WHITE = 7
     STANDARD = 8
 
-class Printer:
-    def __init__(self):
-        pass
-    
-    def printColored(self, msg, color):
-        print(f"{__getColorCode(color)}{msg}{__getColorCode(Color.RESET)}")
 
-    def printProgress(self, progress):
-        pass    # Print a line with a reletive Progress(0-100)
+def printColored(msg, color):
+    print(f"{__getColorCode(color)}{msg}{__getColorCode(Color.STANDARD)}")
+
+def printProgress(progress):
+    pass    # Print a line with a reletive Progress(0-100)
     
-    def __getColorCode(self, color):
-        if not color == Color.STANDARD:
-            return f"\u001b[3{color.value}m"
-        else:
-            return "\u001b[0m"
+def __getColorCode(color):
+    if not color == Color.STANDARD:
+        return f"\u001b[3{color.value}m"
+    else:
+        return "\u001b[0m"
--- core/log.py
+++ core/log.py
@@ -1,31 +1,37 @@
-from io import Printer.printColored, Color
-from enum import Enum
+from .io import Color, printColored
+from enum import IntEnum
+
+'''
+TBD: Log file creation (with the python logging module)
+
+'''
 
 class Logger:
-    class logLevel(Enum):
+
+    class logLevel(IntEnum):
         DEBUG = 0
-        EVENT = 1
+        INFO = 1
         WARNING = 2
         FAILURE = 3
 
     class __Logger:
         def __init__(self, logLevel):
-            setLogLevel(logLevel)
+            self.setLogLevel(logLevel)
 
         def setLogLevel(self, logLevel):
             self.logLevel = logLevel
 
         def d(self, msg):
-            if self.logLevel <= logLevel.DEBUG:
+            if self.logLevel <= Logger.logLevel.DEBUG:
                 printColored(msg, Color.STANDARD)
-        def e(self, msg):
-            if self.logLevel <= logLevel.EVENT:
+        def i(self, msg):
+            if self.logLevel <= Logger.logLevel.INFO:
                 printColored(msg, Color.STANDARD)
         def w(self, msg):
-            if self.logLevel <= logLevel.WARNING:
+            if self.logLevel <= Logger.logLevel.WARNING:
                 printColored(msg, Color.YELLOW)
         def f(self, msg):
-            if self.logLevel <= logLevel.FAILURE:
+            if self.logLevel <= Logger.logLevel.FAILURE:
                 printColored(msg, Color.RED)
 
     instance = None
@@ -33,7 +39,7 @@ class Logger:
     def __init__(self, logLevel = logLevel.WARNING):
         if not Logger.instance:
             Logger.instance = Logger.__Logger(logLevel) #The logLevel is set once during the initial creation of the Logger. Afterwards only an implicit call can change it - not the constructor
-    def __getattribute__(self, name):
-        return getattr(self.instance(name))
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
 
     
