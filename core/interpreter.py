7d46de557d13cd0a46bfc495848de85ec16e6af8
Marco Michl <marco.michl@carissma.eu>
2021-06-16 17:20:00 +0200

    Initiale Erstellung des canBridge-Moduls

--- core/interpreter.py
+++ core/interpreter.py
@@ -1,11 +1,18 @@
-
+from .log import Logger
 
 ''' Simple Interpreter to create a interactive application
 '''
 
 class Interpreter:
     def __init__(self):
+        self.log = Logger()
         self.running = True
+
+    def unknown(self):
+        '''
+        Method to be called if an unknown command was entered
+        '''
+        self.log.w("Unknown Command")
 
     def loop(self):
         while self.running:
---
+++ modules/canBridge.py
@@ -0,0 +1,61 @@
+from core.autosecModule import AutosecModule
+
+
+def load_module():
+    return canBridge()
+
+
+class canBridge(AutosecModule):
+
+    def __init__(self):
+        super.__init__()
+
+        self.interfaces = dict(
+            primaryInterface = dict(name = "primaryInterface",
+                required = True,
+                default = None,
+                unit = "SocketCAN Device Name",
+                range = None,
+                value = None),
+            secondaryInterface = dict(name = "secondaryInterface",
+                required = True,
+                default = None,
+                unit = "SocketCAN Device Name",
+                range = None,
+                value = None)
+            )   
+        self.intercept = [(0x7af, 
+            lambda data: b'\x03\x04\x05',
+            None, 
+            lambda data: b'\x06\x07\x08')]     #list to carry the messages to be intercepted; ID IF1, Answer IF1, ID IF2, Answer IF2
+
+    def getInfo(self):
+        return(dict(
+            name = "canBridge",
+            source = "autosec",
+            type = "mitm",
+            interface = "CAN",
+            description = "Module to perform MITM CAN attacks with two CAN interfaces"))
+
+    def getOptions(self):
+        return self.interfaces #Vielleicht nur eine Kopie, damit setter genutzt werden muss?
+
+    def setOptions(self, options):
+        return super().setOptions(options)
+
+    def run(self):
+        return super().run()
+
+    def forwardMessages():
+        # Sobald Daten auf einem Kanal ankommen prüfen, ob diese verändert werden müssen, sonst weiterleiten
+        pass
+
+    def interceptMessage(filterRule, data):
+        # Abgefangene Nachrichten verändern und dann weiterleiten
+        # Nachricht wurde aufgrund der ID oder des Inhalts identifiziert, nun muss der Inhalt auf eine Nachricht auf eine Antwort je Interface abgebildet werden
+        data = b'\x00\x01\x02'
+
+        primaryAnswer = filterRule[1](data)
+        secondaryAnswer = filterRule[3](data)
+
+        return primaryAnswer, secondaryAnswer
