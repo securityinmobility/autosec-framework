e9b024279572993dfcbdb69b98a52c6beffbe1a7
Marco Michl <marco.michl@carissma.eu>
2021-07-06 15:24:15 +0200

    modified file to fulfill linting requriements

--- core/io.py
+++ core/io.py
@@ -1,6 +1,13 @@
+'''
+This module implements functionality to interact with the user over the cli
+'''
+
 from enum import Enum
 
 class Color(Enum):
+    '''
+    Class that enumerates the different colors that can be used in a basic shell
+    '''
     BLACK = 0
     RED = 1
     GREEN = 2
@@ -12,14 +19,23 @@ class Color(Enum):
     STANDARD = 8
 
 
-def printColored(msg, color):
-    print(f"{__getColorCode(color)}{msg}{__getColorCode(Color.STANDARD)}")
+def print_colored(msg, color):
+    '''
+    Print a message in the shell with the specified color
+    '''
+    print(f"{__get_color_code(color)}{msg}{__get_color_code(Color.STANDARD)}")
 
-def printProgress(progress):
-    pass    # Print a line with a reletive Progress(0-100)
-    
-def __getColorCode(color):
+def print_progress(progress):
+    '''
+    This method shall be used to print a progress from 0-100%
+    '''
+    # Print a line with a reletive Progress(0-100)
+    print(f"{progress}% / 100 %")
+
+def __get_color_code(color):
+    '''
+    Private method that is used to get the color code of a specific color
+    '''
     if not color == Color.STANDARD:
         return f"\u001b[3{color.value}m"
-    else:
-        return "\u001b[0m"
+    return "\u001b[0m"
