64685a894c548c80f079ec89f59238caa38e2279
Marco Michl <marco.michl@carissma.eu>
2021-07-06 14:48:50 +0200

    Changed some files in order to fulfill linting requirements

--- core/app.py
+++ core/app.py
@@ -1,29 +1,42 @@
+""" Main app of the AutoSec framework.
+
+webApi: Not yet implemented, will start a REST API to control the framework
+webApp: Not yet implemented, webApplication that can be used to control the framework.
+
+Requires: webApi = True (error if it is set as false).
+"""
+
 from .log import Logger
 from .interpreter import Interpreter
 
 class App():
+    '''
+    Class that represents the main app
+    '''
     def __init__(self):
-        """ Main app of the AutoSec framework.
-
-        webApi: Not yet implemented, will start a REST API to control the framework
-        webApp: Not yet implemented, webApplication that can be used to control the framework.
-
-        Requires: webApi = True (error if it is set as false).
-        """
+        '''
+        Initializes the main app module and its varibles
+        '''
 
         self.log = Logger()
         self.interpreter = Interpreter()
-        self.webApi = False
-        self.webApp = False
-        self.cliApp = False
+        self.web_api = False
+        self.web_app = False
+        self.cli_app = False
 
     def start(self):
-        if self.webApi:
+        '''
+        Startup method for the app
+        '''
+        if self.web_api:
             self.log.w("Web Api is not yet implemented")
-        if self.webApp:
+        if self.web_app:
             self.log.w("Web App is not yet implemented")
-        if self.cliApp:
+        if self.cli_app:
             self.log.w("CLI App is not yet implemendet")
 
     def stop(self):
+        '''
+        Stop method for the app
+        '''
         self.interpreter.running = False
--- core/core.py
+++ core/core.py
@@ -1,9 +1,28 @@
+'''
+Core Module to organize necessary tasks
+'''
 from core import moduleLoader
 
 class Core:
-    def __init__():
+    '''
+    Class to organize the necessary tasks within the frameworks lifecycle
+    '''
+    def __init__(self):
+        '''
+        Initialize the core module
+        '''
         print("Loading available modules")
         self.modules = moduleLoader.loadAvailableModules()
         print("Loading modules finished")
 
-    
+    def get_moduels(self):
+        '''
+        Method to retrieve the loaded modules
+        '''
+        return self.modules.copy()
+
+    def get_module_number(self):
+        '''
+        Return the number of the loaded modules
+        '''
+        return len(self.modules)
