9ca97ec95b0cbc437f00456940e3176b5503accf
Antonio Paraschiv <anp8919@thi.de>
2021-08-02 22:34:54 +0200

    added check for supported pids

--- autosec/modules/obd.py
+++ autosec/modules/obd.py
@@ -1,6 +1,7 @@
 '''
 Load Obd modules
 '''
+import logging
 from autosec.core.autosec_module import AutosecModule
 from autosec.modules.Obd import service01
 
@@ -16,11 +17,24 @@ class ObdService01(AutosecModule):
     Class that provides the service01 functions.
     '''
     def __init__(self):
+        self.logger = logging.getLogger("autosec.modules.obd")
+        self.logger.setLevel(logging.WARNING)
+
         self.interface = "vcan0"
+
+        self.functions = {
+            "01": service01.get_mil_status,
+            "03": service01.get_fuelsystem_status,
+            "04": service01.get_engine_load,
+            "05": service01.get_engine_coolant_temp,
+            "0C": service01.get_engine_speed,
+            "0D": service01.get_vehicle_speed,
+            "1C": service01.get_obd_standard
+        }
 
     def get_info(self):
         return(dict(
-            name = "obdService01",
+            name = "ObdService01",
             source = "autosec",
             type = "payload",
             interface = "CAN",
@@ -33,12 +47,12 @@ class ObdService01(AutosecModule):
         pass
 
     def run(self):
-        # TODO: Check for available PIDs, then run the matching functions.
-        for func in dir(service01):
-            function = getattr(service01,func)
-            if callable(function) and func.startswith("get") and not func == "get_supported_pid":
-                function(self.interface)
         pids = [0x00, 0x20, 0x40, 0x60, 0x80, 0xA0, 0xC0]
         for pid in pids:
-            service01.get_supported_pid(self.interface, pid)
-        
+            pid_list = service01.get_supported_pid(self.interface, pid)
+            if pid_list is None:
+                self.logger.warning(f"No list returned for PID {hex(pid)}")
+            else:
+                for available_pid in pid_list:
+                    if available_pid in self.functions:
+                        self.functions[available_pid](self.interface)
