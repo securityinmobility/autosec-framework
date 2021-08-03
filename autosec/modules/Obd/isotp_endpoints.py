704dfed94bf43d8e51725649f7c976c1972ab5eb
Antonio Paraschiv <anp8919@thi.de>
2021-08-03 20:02:02 +0200

    added scan for extended IDs

--- autosec/modules/Obd/isotp_endpoints.py
+++ autosec/modules/Obd/isotp_endpoints.py
@@ -3,7 +3,7 @@ Scans for ISO-TP Endpoints
 '''
 import logging
 
-from scapy.all import *
+from scapy.all import conf, load_contrib
 
 logger = logging.getLogger("autosec.modules.Obd.isotp_endpoints")
 logger.setLevel(logging.INFO)
@@ -13,7 +13,15 @@ conf.contribs['CANSocket'] = {'use-python-can': False}
 load_contrib('cansocket')
 load_contrib('isotp')
 
+logger.info("Starting scan for normal IDs...")
 socks = ISOTPScan(CANSocket("vcan0"), range(0x700, 0x7ff), can_interface="vcan0",
                             output_format="text", verbose=True)
-                            # TODO: 29-bit identifier
+logger.info("Scan for normal IDs done.")
+logger.info("Starting scan for extended IDs...")
+socks_extended = ISOTPScan(CANSocket("vcan0"), range(0x700, 0x7ff), can_interface="vcan0",
+                            extended_addressing=True, extended_scan_range=range(0x40, 0x5a),
+                            output_format="text", verbose=True)
+logger.info("Scan for extended IDs done.")
+
 logger.info(socks)
+logger.info(socks_extended)
