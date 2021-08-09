e6ff1524892a42ec4223582b21435aec77571add
Antonio Paraschiv <anp8919@thi.de>
2021-08-09 18:48:42 +0200

    added endpoints scan to module loader

--- autosec/modules/Obd/isotp_endpoints.py
+++ autosec/modules/Obd/isotp_endpoints.py
@@ -5,23 +5,35 @@ import logging
 
 from scapy.all import conf, load_contrib
 
-logger = logging.getLogger("autosec.modules.Obd.isotp_endpoints")
-logger.setLevel(logging.INFO)
+def scan_endpoints(interface):
+    '''
+    Scan for ISO-TP Endpoints
+    '''
+    logger = logging.getLogger("autosec.modules.Obd.isotp_endpoints")
+    logger.setLevel(logging.INFO)
 
-conf.contribs['ISOTP'] = {'use-can-isotp-kernel-module': True}
-conf.contribs['CANSocket'] = {'use-python-can': False}
-load_contrib('cansocket')
-load_contrib('isotp')
+    conf.contribs['ISOTP'] = {'use-can-isotp-kernel-module': True}
+    conf.contribs['CANSocket'] = {'use-python-can': False}
+    load_contrib('cansocket')
+    load_contrib('isotp')
 
-logger.info("Starting scan for normal IDs...")
-socks = ISOTPScan(CANSocket("vcan0"), range(0x700, 0x7ff), can_interface="vcan0",
-                            output_format="text", verbose=True)
-logger.info("Scan for normal IDs done.")
-logger.info("Starting scan for extended IDs...")
-socks_extended = ISOTPScan(CANSocket("vcan0"), range(0x700, 0x7ff), can_interface="vcan0",
-                            extended_addressing=True, extended_scan_range=range(0x40, 0x5a),
-                            output_format="text", verbose=True)
-logger.info("Scan for extended IDs done.")
+    logger.info("Starting scan for normal IDs...")
+    socks = scan_endpoints_normal(interface)
+    logger.info("Scan for normal IDs done.")
+    logger.info(socks)
 
-logger.info(socks)
-logger.info(socks_extended)
+    logger.info("Starting scan for extended IDs...")
+    socks_extended = scan_endpoints_extended(interface)
+    logger.info("Scan for extended IDs done.")
+    logger.info(socks_extended)
+
+def scan_endpoints_normal(interface):
+    socks = ISOTPScan(CANSocket(interface), range(0x700, 0x7ff), can_interface="vcan0",
+                                output_format="text", verbose=True)
+    return socks
+
+def scan_endpoints_extended(interface):
+    socks_extended = ISOTPScan(CANSocket(interface), range(0x700, 0x7ff), can_interface="vcan0",
+                                extended_addressing=True, extended_scan_range=range(0x40, 0x5a),
+                                output_format="text", verbose=True)
+    return socks_extended
--- autosec/modules/obd.py
+++ autosec/modules/obd.py
@@ -3,18 +3,18 @@ Load Obd modules
 '''
 import logging
 from autosec.core.autosec_module import AutosecModule
-from autosec.modules.Obd import service01
+from autosec.modules.Obd import service01, service09, isotp_endpoints
 
 
 def load_module():
     '''
     Method to provide the module to the framework
     '''
-    return [ObdService01()]
+    return [ObdServices(), IsoTpServices()]
 
-class ObdService01(AutosecModule):
+class ObdServices(AutosecModule):
     '''
-    Class that provides the service01 functions.
+    Class that provides the service01 and service09 functions.
     '''
     def __init__(self):
         self.logger = logging.getLogger("autosec.modules.obd")
@@ -34,6 +34,52 @@ class ObdService01(AutosecModule):
 
     def get_info(self):
         return(dict(
+            name = "ObdServices",
+            source = "autosec",
+            type = "payload",
+            interface = "CAN",
+            description = "Module that interprets OBD-II service 01 and 09 PIDs"))
+
+    def get_options(self):
+        return dict(
+            interface = dict(name = "interface",
+                required = True,
+                default = "vcan0",
+                unit = "SocketCAN Device Name",
+                range = None,
+                value = self.interface),
+            )
+
+    def set_options(self, options):
+        self.interface = options
+
+    def run(self):
+        service09.get_vin()
+        self.check_pid_and_run()
+
+    def check_pid_and_run(self):
+        pids = [0x00, 0x20, 0x40, 0x60, 0x80, 0xA0, 0xC0]
+        for pid in pids:
+            pid_list = service01.get_supported_pid(self.interface, pid)
+            if pid_list is None:
+                self.logger.warning(f"No list returned for PID {hex(pid)}")
+            else:
+                for available_pid in pid_list:
+                    if available_pid in self.functions:
+                        self.functions[available_pid](self.interface)
+
+class IsoTpServices(AutosecModule):
+    '''
+    Class that provides the isotp endpoints scan.
+    '''
+    def __init__(self):
+        self.logger = logging.getLogger("autosec.modules.obd")
+        self.logger.setLevel(logging.WARNING)
+
+        self.interface = "vcan0"
+
+    def get_info(self):
+        return(dict(
             name = "ObdService01",
             source = "autosec",
             type = "payload",
@@ -48,18 +94,10 @@ class ObdService01(AutosecModule):
                 unit = "SocketCAN Device Name",
                 range = None,
                 value = self.interface),
-            )   
+            )
 
     def set_options(self, options):
         self.interface = options
 
     def run(self):
-        pids = [0x00, 0x20, 0x40, 0x60, 0x80, 0xA0, 0xC0]
-        for pid in pids:
-            pid_list = service01.get_supported_pid(self.interface, pid)
-            if pid_list is None:
-                self.logger.warning(f"No list returned for PID {hex(pid)}")
-            else:
-                for available_pid in pid_list:
-                    if available_pid in self.functions:
-                        self.functions[available_pid](self.interface)
+        isotp_endpoints.scan_endpoints(self.interface)
