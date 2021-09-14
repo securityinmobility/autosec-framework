a74053689abe34395df826c30f8823dbd5ec7ae2
Christa Strauß <chs6551@thi.de>
2021-09-14 15:31:26 +0200

    [skip ci] added editing of the prompts

--- autosec/modules/iPythonConfig.py
+++ autosec/modules/iPythonConfig.py
@@ -1,12 +1,28 @@
+'''
+This is where the configurations for the (Core-)IPython-UI can be made. 
+'''
 from traitlets.config import Config
 from IPython.terminal.prompts import Prompts, Token
-import os
 
 class IPythonConfig(Config):
-
-
+    '''
+    Class to edit all IPython configurations
+    '''
     def __init__(self):
-        self.InteractiveShellApp.exec_lines= [
+        # set the prompts
+        self.InteractiveShell.prompts_class = UIPrompt
 
-        ]
- 
+
+class UIPrompt(Prompts):
+    '''
+    Class that provides functions to edit the in / out / continuation / rewrite 
+    prompts for the ui. 
+    '''
+    def in_prompt_tokens(self, cli=None):
+        return [(Token.Prompt, '>>>')]
+
+    # def continuation_prompt_tokens(self, cli=None, width=None):
+
+    # def rewrite_promt_tokens():
+
+    # def out_prompt_tokens():
