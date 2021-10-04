a400868586711fb2c47fa9c03291af653a620bb7
Jakob Löw <jakob@m4gnus.de>
2021-10-04 17:42:38 +0200

    :art: :rotating_light: not even sure if the can_bridge module works like this, but now it at least does not contain any pylint errors

--- autosec/modules/can_bridge.py
+++ autosec/modules/can_bridge.py
@@ -1,13 +1,14 @@
 '''
 Module to provide sniffing and manipulation of CAN networks.
-This attack works as a MITM with two CAN interfaces. 
-Usually, all messages are transferred between the interfaces, but some 
+This attack works as a MITM with two CAN interfaces.
+Usually, all messages are transferred between the interfaces, but some
 messages (e.g. with certain IDs) can be manipulated.
 '''
 import threading
 import logging
+from scapy.all import CAN, CANSocket, load_layer, load_contrib
+
 from autosec.core.autosec_module import AutosecModule
-from scapy.all import *
 
 def load_module():
     '''
@@ -38,16 +39,17 @@ class CanBridge(AutosecModule):
                 unit = "SocketCAN Device Name",
                 range = None,
                 value = None)
-            )   
-        
+            )
+
         load_layer("can")
         load_contrib("cansocket")
 
         self.primary_interface = CANSocket(channel = "vcan0")
         self.secondary_interface = CANSocket(channel = "vcan1")
         self.filters = ([], [])
+        self.thread = None
 
-    def getInfo(self):
+    def get_info(self):
         return(dict(
             name = "canBridge",
             source = "autosec",
@@ -55,11 +57,11 @@ class CanBridge(AutosecModule):
             interface = "CAN",
             description = "Module to perform MITM CAN attacks with two CAN interfaces"))
 
-    def getOptions(self):
-        return self.interfaces.copy() 
+    def get_options(self):
+        return self.interfaces.copy()
 
-    def setOptions(self, options):
-        return super().set_options(options)
+    def set_options(self, options):
+        raise NotImplementedError
 
     def run(self):
         self.logger.debug("Starting the bridge..")
@@ -67,28 +69,36 @@ class CanBridge(AutosecModule):
         self.thread.start()
 
     def _sniff(self):
-        self.primary_interface.sniff(prn=lambda x: self._primary_message(x))
-        self.secondary_interface.snitt(prn=lambda x: self._secondary_message(x))
+        self.primary_interface.sniff(prn=self._primary_message)
+        self.secondary_interface.snitt(prn=self._secondary_message)
 
     def _primary_message(self, pkt):
         self._on_message(0, pkt.identifier, pkt.data)
-    
+
     def _secondary_message(self, pkt):
         self._on_message(1, pkt.identifier, pkt.data)
 
-    def _on_message(self,interface, id, data):
-        '''self.intercept = [(0x7af, 
-            lambda data: b'\x03\x04\x05',
-            None, 
-            lambda data: b'\x06\x07\x08')]     #list to carry the messages to be intercepted; ID IF1, Answer IF1, ID IF2, Answer IF2eceived message.
+    def _on_message(self, interface, msg_id, data):
+        '''
+        # list to carry the messages to be intercepted
+        # ID IF1, Answer IF1, ID IF2, Answer IF2eceived message.
+        self.intercept = [
+            (
+                0x7af,
+                lambda data: b'\x03\x04\x05',
+                None,
+                lambda data: b'\x06\x07\x08'
+            )
+        ]
         interface: interface that received message (0 = primary, 1 = secondary)
         id: ID of the received message
         data: data of the received message
         '''
-        self.logger.debug(f"Received message on {interface} with id {id} and data {data}")
+        self.logger.debug("Received message on %s with id %03x and data %s",
+            interface, msg_id, data)
         result = (False, None, None)
-        for filter in self.filters[interface]:
-            filter_result = filter(id, data)
+        for _filter in self.filters[interface]:
+            filter_result = _filter(msg_id, data)
             if filter_result[0]:
                 result = filter_result
 
@@ -97,28 +107,16 @@ class CanBridge(AutosecModule):
             self._send(self.secondary_interface, result[2])
         else:
             if interface == 0:
-                self._send(self.secondary_interface, (id, data))
+                self._send(self.secondary_interface, (msg_id, data))
             else:
-                self._send(self.primary_interface, (id, data))
-    
-    def _create_can_socket(self, interface, device):
-        '''
-        Creates the socket for the specified device
-        '''
-        try:
-            interface = CANSocket(channel = device)
-            return
-        except OSError as error:
-            self.logger.warning(f"Could not create CAN Socket. No such device: {device}")
-        interface = None
+                self._send(self.primary_interface, (msg_id, data))
 
-
-    @classmethod
-    def _send(self, interface, packet):
+    @staticmethod
+    def _send(interface, packet):
         '''
         Method to send a packet on a specified interface
         '''
-        if not packet == None:
-            id = packet[0]
+        if packet is not None:
+            msg_id = packet[0]
             data = packet[1]
-            interface.send(CAN(identifier=id, length=len(data), data=data))
+            interface.send(CAN(identifier=msg_id, length=len(data), data=data))
