e2a967d0f1b36ec35c0aa7015f45c976430243bd
ldietl <88787138+ldietl@users.noreply.github.com>
2022-05-09 17:45:09 +0200

    Added port-scanner module
    Added port-scanner module and fixed
    arp_scan.py

--- autosec/modules/arp_scan.py
+++ autosec/modules/arp_scan.py
@@ -1,6 +1,9 @@
 from scapy.all import ARP, Ether, srp, in6_isvalid
-from autosec.core.autosec_module import AutosecModule
-from autosec.core.ressources import ip
+from autosec.core.autosec_module import AutosecModule, AutosecModuleInformation
+from autosec.core.ressources import ip, InternetDevice, InternetInterface
+from typing import List
+
+from autosec.core.ressources.base import AutosecRessource
 
 
 def load_module():
@@ -9,7 +12,11 @@ def load_module():
     '''
     return [ArpService()]
 
+
 class ArpService(AutosecModule):
+    '''
+    Class that provides ARP scan for a network
+    '''
 
     def __init__(self):
         super().__init__()
@@ -17,30 +24,30 @@ class ArpService(AutosecModule):
         self.ether_broadcast = "ff:ff:ff:ff:ff:ff"
 
     def get_info(self):
-        return (dict(
+        return AutosecModuleInformation(
             name = "arpScan",
-            source = "autosec",
-            type = "arping",
-            interface = "IP",
-            description = "Module to perform ARP scanning IPs in a network."
-        ))
+            description = "Module to perform ARP scanning IPs in a network.",
+            dependencies = [],
+            tags = ["IP","ARP","Scan"]
+        )
     
-    def get_produced_outputs(self):
-        return [ip.InternetDevice]
+    def get_produced_outputs(self) -> List[InternetDevice]:
+        return [InternetDevice]
     
 
-    def get_required_ressources(self):
-        return [self.get_ressource([ip.InternetInterface])]
+    def get_required_ressources(self) -> List[AutosecRessource]:
+        return [InternetInterface]
     
     
-    def run(self, inputs):
+    def run(self,inputs: List[AutosecRessource]) -> List[InternetDevice]:
+        interface = self.get_ressources(inputs, InternetInterface)
         results = []
-        for input in inputs:
+        for input in interface:
             arp_request = ARP(pdst=input.get_address())
             broadcast = Ether(dst=self.ether_broadcast)
             arp_request_broadcast = broadcast/arp_request
             answered, _ = srp(arp_request_broadcast, timeout=2)
-            result = [ip.InternetDevice(ipv6=received.psrc) if (in6_isvalid(received)) else ip.InternetDevice(ipv4=received.psrc)  for received,_ in answered]
+            result = [InternetDevice(ipv6=received.psrc) if (in6_isvalid(received)) else InternetDevice(ipv4=received.psrc)  for received,_ in answered]
             results += result
         return results
 
---
+++ autosec/modules/port_scan.py
@@ -0,0 +1,51 @@
+from scapy.all import RandShort, conf, sr1, IP, TCP, in6_isvalid, L3RawSocket
+from autosec.core.autosec_module import AutosecModule, AutosecModuleInformation
+from autosec.core.ressources import ip, AutosecRessource, InternetDevice, InternetService, InternetInterface
+from typing import List
+
+
+def load_module():
+    '''
+    Method to provide the module to the framework
+    '''
+    return [PortService()]
+
+class PortService(AutosecModule):
+    '''
+    Class that provides port scanning for an ip address.
+    '''
+
+    def __init__(self):
+        super().__init__()
+
+    def get_info(self):
+        return AutosecModuleInformation(
+            name = "portScan",
+            description = "Module to perform scanning for open ports for a given network device.",
+            dependencies=[],
+            tags = ["IP", "Port", "Scan"],
+        )
+    
+    def get_produced_outputs(self) -> List[InternetService]:
+        return [InternetService]
+    
+
+    def get_required_ressources(self) -> List[AutosecRessource]:
+        return [InternetInterface, InternetDevice]
+    
+    
+    def run(self, inputs: List[AutosecRessource]) -> List[InternetService]:
+        conf.L3socket = L3RawSocket
+        device = self.get_ressource(inputs, InternetDevice)
+        ip_address = device.get_address()
+        src_port = RandShort()
+        open_ports = []
+        for port in range(1, 65536):
+            ipPkt = IP(dst=ip_address)
+            tcpPkt = TCP(sport=src_port, dport=port, flags="S")
+            response = sr1(ipPkt/tcpPkt, timeout=15, verbose = 0)
+            if response is not None and response.haslayer(TCP):
+                if response.getlayer(TCP).flags == "SA":
+                    open_ports.append(InternetService(device= InternetDevice(ipv6=ip_address) if (in6_isvalid(ip_address)) else ip.InternetDevice(ipv4=ip_address), 
+                                                        port=port))
+        return open_ports
