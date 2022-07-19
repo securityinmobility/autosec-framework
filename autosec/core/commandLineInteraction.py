e597d16e380002b67b8fc8042fc25f162a763649
Molodos <michael/weichenriedert/online.de>
2022-07-19 20:12:49 +0200

    Fixed commandline interaction for further usage

--- autosec/core/commandLineInteraction.py
+++ autosec/core/commandLineInteraction.py
@@ -1,17 +1,19 @@
-from autosec import userInteraction
 import sys
 from typing import List
+
+from autosec.core.userInteraction import userInteraction
+
 
 class CommandLineInteracton(userInteraction):
     """
     class that provides a command line user interaction interface
     """
-    def __ini__(self):
+
+    def __init__(self):
         super().__init__()
 
-
     def yesNoAnswer(self) -> str:
-        print(self.getQuestion() + "\t [y]es, [n]o \n")
+        print(self.getQuestion(self) + "\t [y]es, [n]o \n")
         answer = str(sys.argv[1])
         if answer not in ["y", "n"]:
             print("Please choose from: [y]es, [n]o \n")
@@ -20,13 +22,13 @@ class CommandLineInteracton(userInteraction):
         return answer
 
     def stringAnswer(self) -> str:
-        print(self.getQuestion() + "\n")
+        print(self.getQuestion(self) + "\n")
         answer = str(sys.argv[1])
         self._answer = answer
         return answer
 
     def integerAnswer(self) -> int:
-        print(self.getQuestion() + "\n")
+        print(self.getQuestion(self) + "\n")
         try:
             answer = int(sys.argv[1])
         except:
@@ -36,17 +38,16 @@ class CommandLineInteracton(userInteraction):
         return answer
 
     def checkListAnswer(self, inputs: List[str]) -> str:
-        print(self.getQuestion() + "\n")
+        print(self.getQuestion(self) + "\n")
         print("Options to choose from: \n")
         for option in range(len(inputs)):
-            print("%s \t %s" %(option, inputs[option]))
+            print("%s \t %s" % (option, inputs[option]))
         answer = int(sys.argv[1])
         if answer not in [i for i in range(len(inputs))]:
-            print("Please only select number between %s and %s \n" %(0, len(inputs)))
+            print("Please only select number between %s and %s \n" % (0, len(inputs)))
             answer = self.checkListAnswer(inputs)
         self._answer = answer
         return answer
 
-    def feedback(self, feedback:str):
+    def feedback(self, feedback: str):
         print(feedback)
-            
